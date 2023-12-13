0fb787cca3f402a62e321b386ddf8b8f137584e9
Vitor Vilarim <vitordaniel325@gmail.com>
2023-12-12 21:33:08 -0400

    controlador para escolher horario do evento foi criado

--- src/routes/route.js
+++ src/routes/route.js
@@ -1,9 +1,10 @@
 const express = require('express');
-const { listarEventos, escolherEventos } = require('../controllers/eventos');
+const { listarEventos, escolherEventos, escolherHorario } = require('../controllers/eventos');
 const rotas = express();
 
 
 rotas.get('/eventos', listarEventos);
 rotas.get('/evento', escolherEventos);
+rotas.get('/evento/:id', escolherHorario);
 
 module.exports = rotas;
