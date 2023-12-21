6f8290a4596bfc9ff87a89aca72704e4ef15046e
Vitor Vilarim <132966937+vitorvilarim@users.noreply.github.com>
2023-12-20 21:02:44 -0400

    integração com a stripe foi criada assim como a implementação do controlador para realizar vendas

---
+++ src/controllers/vendas.js
@@ -0,0 +1,44 @@
+const knex = require('../config/conection');
+const { criarToken, cobrar } = require('../api/gateway');
+
+const venda = async (req, res) => {
+    const { evento_id, quantidade, card } = req.body;
+
+    try {
+        const produto = await knex('eventos').where({ id: evento_id }).first();
+
+        if (!produto) {
+            return res.status(404).json({ mensagem: "evento não existe" });
+        }
+
+        if (quantidade < 1) {
+            return res.status(400).json({ mensagem: " A quantidade é de no mínimo 1" });
+        }
+
+        const valorvenda = produto.preco * quantidade;
+
+        // A linha a baixo só funciona para transações reais
+        // const tokenCartao = await criarToken({ card });
+        const cobranca = await cobrar(valorvenda, 'tok_visa');
+
+        const inserirDados = {
+            id: req.usuario.id,
+            evento_id,
+            quantidade,
+            transacao_id: cobranca.id
+        }
+        const vendaRealizada = await knex('vendas').insert(inserirDados).returning('*');
+        return res.status(201).json(cobranca);
+    } catch (error) {
+        if (error.response) {
+            return res
+                .status(400)
+                .json({ mensagem: error.response.data.error.message })
+        }
+        return res.status(500).json({ mensagem: 'Erro interno do servidor' })
+    }
+}
+
+module.exports = {
+    venda
+}
